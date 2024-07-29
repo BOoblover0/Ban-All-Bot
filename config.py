91deffab4192f83272d2156ba4db435fb36a6cad
BOoblover0 <weblogin4333@gmail.com>
2024-07-29 17:00:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=1619182628
-    API_HASH="ksjs"
-    TOKEN="jaksks"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "5264285143").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=1013
-    BOT_USERNAME="djsjw"
-    BOT_NAME="BAN ALL"
+    API_ID=016675873641
+    API_HASH="3fc2b371f4fbb0166758736414d8be92"
+    TOKEN="6882588183:AAGjJpJz_nOxYwu-uoEgOux7R8dt9zThSUM"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "1266240012").split(" "))
+    START_IMG="https://graph.org/file/e7a7c584fc293ed519433.jpg"
+    BOT_ID=6882588183
+    BOT_USERNAME="@ChocoX911_bot"
+    BOT_NAME="Choco"
